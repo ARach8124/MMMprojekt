06f93094a7576951a6dca3a57e3bf8ef33aac24d
ARach8124 <antrach.8124@gmail.com>
2026-03-12 01:47:51 +0100

    modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 python -m venv .venv
 .\.venv\Scripts\Activate.ps1
 pip install numpy matplotlib scipy
+
+na potem
+rodzaj_sygnalu=int(input("Podaj rodzaj sygnału:\n 1-Prostokątny\n 2-Trójkątny\n 3-Sinusoidalny"))
+def pochodne(t, y, i_L):
+u=uwejsciowe(t)
--- main.py
+++ main.py
@@ -2,5 +2,22 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 import scipy
+#wartości testowe potem będzie input
+rodzaj_sygnalu=1
+f=50
 
-def 
+
+
+#
+def uwejsciowe(t):
+    omega=f*2*np.pi
+    if rodzaj_sygnalu==1:
+        
+
+
+
+
+    
+
+    
+
