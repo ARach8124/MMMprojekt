dc92ce70d9417a8d9c2c8a7a8db14da9aececd38
ARach8124 <antrach.8124@gmail.com>
2026-03-12 12:30:03 +0100

    modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ na potem
 rodzaj_sygnalu=int(input("Podaj rodzaj sygnału:\n 1-Prostokątny\n 2-Trójkątny\n 3-Sinusoidalny"))
 def pochodne(t, y, i_L):
 u=uwejsciowe(t)
+t=np.linspace(0,zadanyczas,1000)
--- main.py
+++ main.py
@@ -4,14 +4,44 @@ import matplotlib.pyplot as plt
 import scipy
 #wartości testowe potem będzie input
 rodzaj_sygnalu=1
-f=50
-
+f=4
+zadanyczas=3
+krok=0.002
 
 
 #
 def uwejsciowe(t):
     omega=f*2*np.pi
     if rodzaj_sygnalu==1:
+        u=signal.square(omega*t)
+        return u
+    elif rodzaj_sygnalu==2:
+        u=signal.sawtooth(omega*t,width=0.5)
+        return u
+    elif rodzaj_sygnalu==3:
+        u=np.sin(omega*t)
+        return u
+    else:
+        return
+
+def sygnalwejsciowy(f):
+    omega=f*2*np.pi
+    t=np.arange(0,zadanyczas,krok)
+    if rodzaj_sygnalu==1:
+        u=signal.square(omega*t)
+        return u
+    elif rodzaj_sygnalu==2:
+        u=signal.sawtooth(omega*t,width=0.5)
+        return u
+    elif rodzaj_sygnalu==3:
+        u=np.sin(omega*t)
+        return u
+    else:
+        return
+
+plt.plot(sygnalwejsciowy(f))
+plt.show()    
+        
         
 
 
